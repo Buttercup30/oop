3d7daf020f3f8311d7aa5cc30a68dcf7145e3d9f
Buttercup30 <buttercup073004@gmail.com>
2026-01-20 23:17:58 +0600

    4th hackerrank problem solve in oop

---
+++ Table.java
@@ -0,0 +1,18 @@
+package oop;
+import java.util.*;
+ class PrintTable{
+    void table(){
+        Scanner sc= new Scanner(System.in);
+        int N =sc.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(N + " x " + i + " = " + N*i );
+        }
+        sc.close();
+    }
+ }
+public class Table {
+    public static void main(String[] args) {
+        PrintTable obj =new PrintTable();
+        obj.table();
+    }
+}
